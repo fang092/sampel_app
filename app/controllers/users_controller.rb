d729618326b016e759051e98ae388748cd51c13e
mclacs14 <bradleyg36@gmail.com>
2014-12-02 10:49:22 -0500

    Started admin rights

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
- before_action :logged_in_user, only: [:index,:edit, :update]
+ before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
  before_action :correct_user,   only: [:edit, :update]
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
   def index
     @users = User.paginate(page: params[:page])
   end
