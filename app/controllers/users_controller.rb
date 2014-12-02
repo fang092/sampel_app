a2a523eae81a7a9fc49a37e85be5210fdfc41f3b
mclacs14 <bradleyg36@gmail.com>
2014-12-02 13:49:18 -0500

    Found the bug and Finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
  before_action :correct_user,   only: [:edit, :update]
+ before_action :admin_user,     only: :destroy
+
+
 
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -64,5 +67,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user == current_user
+    end
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
